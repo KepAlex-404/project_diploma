ac80110d36b622c1901d7681fcf49a65f57ad269
Oleksii Gryshyn <oleksii.gryshyn@nordstrom.com>
2023-05-03 18:29:17 +0300

    exception handler and docs

--- src/api/routers/Dependency.py
+++ src/api/routers/Dependency.py
@@ -9,6 +9,7 @@ from passlib.context import CryptContext
 # to get a string like this run:
 # openssl rand -hex 32
 from src.api.utils.DataBodies import UserInDB, User, TokenData, Token
+from src.api.utils.Exceptions import credentials_exception
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -70,11 +71,6 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -100,11 +96,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
 
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        raise credentials_exception
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
